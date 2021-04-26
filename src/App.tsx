efd229c8c1b0390b08bf1d016eb7ef0d676e3ef3
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-04-26 15:07:15 +0200

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
 
 function App() {
-  return <h1>Hello Ludo!</h1>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={IndexPage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;

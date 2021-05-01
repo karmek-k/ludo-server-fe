0d9be14e1fee55d02d8874fabbdbea9ecdec2f44
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-05-01 17:57:01 +0200

    Remove logic from ConnectionIndicator

--- src/components/shared/ConnectionIndicator.tsx
+++ src/components/shared/ConnectionIndicator.tsx
@@ -1,9 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core';
-import Context from '../../utils/context';
 import { ConnectionState, indicatorMessages } from '../../utils/socketio';
+
+interface Props {
+  connected: boolean;
+}
 
 const useStyles = makeStyles({
   paper: {
@@ -23,34 +26,19 @@ const useStyles = makeStyles({
   }
 });
 
-const ConnectionIndicator: React.FC = () => {
-  const { socket } = useContext(Context);
-  const [connection, setConnection] = useState<ConnectionState>(
-    ConnectionState.DISCONNECTED
-  );
-
-  useEffect(() => {
-    if (socket?.disconnected || !socket) {
-      setConnection(ConnectionState.DISCONNECTED);
-    }
-
-    if (socket?.connected) {
-      setConnection(ConnectionState.CONNECTED);
-    }
-  }, [socket]);
+const ConnectionIndicator: React.FC<Props> = ({ connected }) => {
+  const state = connected
+    ? ConnectionState.CONNECTED
+    : ConnectionState.DISCONNECTED;
 
   const classes = useStyles();
 
   return (
     <Paper className={classes.paper} elevation={4}>
       <Typography
-        className={
-          connection === ConnectionState.CONNECTED
-            ? classes.connected
-            : classes.disconnected
-        }
+        className={connected ? classes.connected : classes.disconnected}
       >
-        {indicatorMessages.get(connection)}
+        {indicatorMessages.get(state)}
       </Typography>
     </Paper>
   );

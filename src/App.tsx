d7a6bd7074f3a392a00d564167bd58f6eae295bf
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-05-01 18:14:15 +0200

    Connect to a sample server url

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 import IndexPage from './pages/IndexPage';
 import GamePage from './pages/GamePage';
 import NotFoundPage from './pages/NotFoundPage';
 import Context from './utils/context';
 import ConnectionIndicator from './components/shared/ConnectionIndicator';
 
+const serverUrl = 'http://localhost:8000';
+
 const App: React.FC = () => {
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [connected, setConnected] = useState<boolean>(false);
+
+  useEffect(() => {
+    const s = io(serverUrl);
+
+    s.on('connect', () => {
+      setConnected(true);
+    });
+    s.on('disconnect', () => {
+      setConnected(false);
+    });
+
+    setSocket(s);
+  }, []);
+
   return (
-    <Context.Provider value={{ socket: null }}>
+    <Context.Provider value={{ socket }}>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={IndexPage} />
@@ -17,7 +35,7 @@ const App: React.FC = () => {
           <Route component={NotFoundPage} />
         </Switch>
       </BrowserRouter>
-      <ConnectionIndicator connected={false} />
+      <ConnectionIndicator connected={connected} />
     </Context.Provider>
   );
 };

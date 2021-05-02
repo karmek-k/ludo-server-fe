5fc575c516a8c1ab4ee337d024804b6d1d5bd472
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-05-02 11:26:02 +0200

    Preserve state when dispatching

--- src/utils/reducers.ts
+++ src/utils/reducers.ts
@@ -6,8 +6,8 @@ export enum ServerUrlAction {
 export const reducer = (state: any, action: any): any => {
   switch (action.type) {
     case ServerUrlAction.SET:
-      return { serverUrl: action.serverUrl };
+      return { ...state, serverUrl: action.serverUrl };
     case ServerUrlAction.RESET:
-      return { serverUrl: '' };
+      return { ...state, serverUrl: '' };
   }
 };
